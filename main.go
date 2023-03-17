3963583a8d1d6b8089b7ec0bf66c9e830aafa320
raggaboy <kolesolek97@gmail.com>
2023-03-17 16:14:20 +0700

    commit init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"main/cache"
+)
+
+func main() {
+	cache := cache.New()
+
+	cache.Set("userId", 42)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+	userId = cache.Get("userId")
+
+	fmt.Println(userId)
+}
